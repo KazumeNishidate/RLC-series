6ac3162518f2c98a0c32b6ea50c2dfcc5eafe7bb
Kazume Nishidate <kazume.nishidate@gmail.com>
2026-03-28 00:55:42 +0900

    test

--- rlc-series.c
+++ rlc-series.c
@@ -25,10 +25,10 @@ int jac(double t, const double y[], double *dfdy,
   gsl_matrix_view dfdy_mat
     = gsl_matrix_view_array (dfdy, 2, 2);
   gsl_matrix * m = &dfdy_mat.matrix;
-  gsl_matrix_set(m, 0, 0,  1.0/CC);
-  gsl_matrix_set(m, 0, 1,     0.0);
-  gsl_matrix_set(m, 1, 0,  -RR/LL);
-  gsl_matrix_set(m, 1, 1, -1.0/LL);
+  gsl_matrix_set(m, 0, 0,  -RR/LL);
+  gsl_matrix_set(m, 0, 1, -1.0/LL);
+  gsl_matrix_set(m, 1, 0,  1.0/CC);
+  gsl_matrix_set(m, 1, 1,     0.0);
   dfdt[0] = 0.0;
   dfdt[1] = 0.0;
   return GSL_SUCCESS;
